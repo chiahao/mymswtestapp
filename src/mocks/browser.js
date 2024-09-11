59980bb37fd3f09a6ca7f814bb5e718d93431679
chiahao <hao.wu1980@gmail.com>
2024-09-11 09:27:10 +0800

    feat: add debug output
    Add console output for every request input.

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -2,3 +2,13 @@ import { setupWorker } from "msw/browser";
 import { handlers } from "./handler";
 
 export const worker = setupWorker(...handlers);
+worker.events.on('request:start', async ({ request }) => {
+  // Read the request body as text for every request
+  // that occurs in your application.
+  const payload = await request.clone().text()
+});
+
+worker.events.on('request:start', ({ request }) => {
+  console.log('Outgoing:', request.method, request.url)
+});
+
